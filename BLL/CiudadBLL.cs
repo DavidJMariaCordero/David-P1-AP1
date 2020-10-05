533a2c2b9dbef43fcf8068cfc795e206ae9a6990
David Maria <eldavid_46@hotmail.com>
2020-10-05 19:26:38 -0400

    Avance Con la BLL y el rCiudad.xam.cs

---
+++ BLL/CiudadBLL.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using David_P1_AP1;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using David_P1_AP1.DAL;
+using David_P1_AP1.Entidades;
+
+namespace David_P1_AP1.BLL{
+    public class CiudadBLL{
+        public static bool Guardar(Ciudad ciudad){
+            if(!Existe(ciudad.CiudadId))
+                return Insertar(ciudad);
+            else 
+                return Modificar(ciudad);
+        }
+
+        public static bool Insertar(Ciudad ciudad){
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try{
+                contexto.Ciudad.Add(ciudad);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch(Exception){
+                throw;
+            }
+            finally{
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Ciudad ciudad){
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try{
+                contexto.Entry(ciudad).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch(Exception){
+                throw;
+            }
+            finally{
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id){
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try{
+                var ciudad = contexto.Ciudad.Find(id);
+                if(ciudad != null){
+                    contexto.Ciudad.Remove(ciudad);
+                    paso = contexto.SaveChanges() > 0;     
+                }
+                
+            }
+            catch(Exception){
+                throw;
+            }
+            finally{
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Ciudad Buscar(int id){
+            Ciudad ciudad;
+            Contexto contexto = new Contexto();
+
+            try{
+                ciudad = contexto.Ciudad.Find(id);
+                
+            }
+            catch(Exception){
+                throw;
+            }
+            finally{
+                contexto.Dispose();
+            }
+            return ciudad;
+        }
+
+        public static bool Existe(int id){
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+            try{
+                encontrado = contexto.Ciudad.Any(c => c.CiudadId == id);
+            }
+            catch(Exception){
+                throw;
+            }
+            finally{
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+
+        
+    }
+}
---
+++ UI/Registros/rCiudad.xaml.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Windows;
+using David_P1_AP1.DAL;
+using David_P1_AP1.Entidades;
+using David_P1_AP1.BLL;
+using David_P1_AP1.UI.Registros;
+
+namespace David_P1_AP1.UI.Registros{
+    public partial class rCiudad:Window{
+        private Ciudad ciudad;
+        public rCiudad(){
+            InitializeComponent();
+            ciudad = new Ciudad;
+            this.DataContext = ciudad;
+        }
+
+        public void GuardarBoton_Click(object render, RoutedEventArgs e){
+            if(!Validar())
+                return;
+            var paso = CiudadBLL.Guardar(ciudad);
+            if(paso){
+                Limpiar();
+                MessageBox.Show("Guardado con exito", "Bien", MessageBoxButton.Ok);
+            }
+            else
+            {
+                MessageBox.Show("Error al guardar", "Error", MessageBoxButton.Ok);
+            }
+
+        }
+
+        public void Limpiar(){
+            this.ciudad = new Ciudad();
+            this.DataContext = ciudad;
+
+        }
+
+        public void BuscarBoton_Click(object render, RoutedEventArgs e){
+            Contexto contexto = new Contexto();
+            var encontrado = CiudadBLL.Buscar(Convert.ToInt32(CiudadIdTextBox.Text));
+            if(encontrado != null)
+                this.ciudad = encontrado;
+            else{
+                this.ciudad = new Ciudad();
+                MessageBox.Show("No encontrado", "Error", MessageBoxButton.OK);
+            }
+
+            this.DataContext = this.ciudad;
+        }
+
+        private bool Validar(){
+            bool valido = true;
+            if(NombreTextBox.Text.Lenght == 0){
+                valido = false;
+                MessageBox.Show("Debe incluir el nombre", "Error", MessageBoxButton.OK);
+            }
+            return valido;
+        }
+
+        public void NuevoBoton_Click(object render, RoutedEventArgs e){
+            Limpiar();
+        }
+
+        public void EliminarBoton_Click(object render, RoutedEventArgs e){
+            if(CiudadBLL.Eliminar(Convert.ToInt32(CiudadIdTextBox.Text))){
+                Limpiar();
+                MessageBox.Show("Eliminado", "Exito", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show("Error al eliminar", "Error", MessageBoxButton.OK);
+            }
+        }
+
+    }
+}
